3bc481d92b966576954163147b8a81dafb933269
Juan Tobar <juanpablococtm@gmail.com>
2025-03-11 22:09:58 -0500

    Agregar funcionalidad para obtener publicaciones del blog y crear una nueva página de dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardRote() {
+  return (
+    <div>
+      <h1>Hello from the dashboard</h1>
+    </div>
+  )
+}
+
+// documentar como inciar la base de datos con prisma
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
-import Link from 'next/link'
+import { prisma } from "./utils/db";
 
-export default function Home() {
+async function getData() {
+  
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true,
+      createdAt: true,
+    }
+  })
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <div className='py-6'>
-      <h1 className='text-3xl font-bold tracking-tight mb-8'>Latest posts</h1>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-        {/* minuto 49 fetching data */}
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {data.map((item) => (
+          <h1 key={item.title} >{item.title}</h1>
+        ))}
+          
+        
       </div>
     </div>
   );
